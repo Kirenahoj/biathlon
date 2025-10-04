6aca8f045d833988e64ccdac506e04c18d0fa35d
Erik <erik.bromark.8255@student.uu.se>
2025-10-04 02:57:43 +0200

    Klar med grunden

--- biathlon.py
+++ biathlon.py
@@ -9,3 +9,49 @@ def splash():
     print("##         a hit or miss game          ##")
     print("#########################################")
     print()
+
+def is_open(target):
+    if target == open:
+        return True
+    else:
+        return False
+    
+def is_closed(target):
+    if target == closed:
+        return True
+    else:
+        return False
+    
+def new_targets():
+    targets = []
+    for _ in range(5):
+        targets.append(open)
+    return targets
+
+def close_target(targets, position):
+    targets[position] = closed
+    return targets
+
+def points(targets):
+    n = 0
+
+    for target in targets:
+        if is_closed(target):
+            n = n+1
+    return n
+
+def targets_to_string(targets):
+    string = "     "
+    for target in targets:
+        if is_closed(target):
+            string = string + f"* "
+        else:
+            string = string + f"O "
+    return string
+
+def view_targets(targets):
+    print()
+    print("     1 2 3 4 5")
+    print()
+    print(targets_to_string(targets))    
+    print()
